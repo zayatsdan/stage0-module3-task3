c9e5d350109f822d49cadcfef17b7789e2ac00fc
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-15 16:31:37 +0100

    Update BasicVariablesInitialization.java

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100; 
     }
 }
